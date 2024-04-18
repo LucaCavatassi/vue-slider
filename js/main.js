5d2408fba79434e087dee787c0a38f39446c9938
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-04-18 17:02:48 +0200

    feat: thumbnail working (che fatica)

--- js/main.js
+++ js/main.js
@@ -1,50 +1,3 @@
-// const images = [
-//   {
-//     image: "img/01.webp",
-//     title: "Marvel's Spiderman Miles Morale",
-//     text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-//   },
-//   {
-//     image: "img/02.webp",
-//     title: "Ratchet & Clank: Rift Apart",
-//     text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-//   },
-//   {
-//     image: "img/03.webp",
-//     title: "Fortnite",
-//     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-//   },
-//   {
-//     image: "img/04.webp",
-//     title: "Stray",
-//     text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-//   },
-//   {
-//     image: "img/05.webp",
-//     title: "Marvel's Avengers",
-//     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-//   },
-// ];
-
-// let activeIndex = 0;
-
-
-
-// function showPrev () {
-//   imageElems[activeIndex].classList.remove("active")
-//   thumbnailElems[activeIndex].classList.remove("active")
-
-
-//   if (activeIndex > 0){
-//     activeIndex--;
-//   } else {
-//     activeIndex = imageElems.length - 1;
-//   }
-
-//   imageElems[activeIndex].classList.add("active")
-//   thumbnailElems[activeIndex].classList.add("active");
-// }
-
 // function Stop(){
 //   startStopBtn.removeEventListener("click", Stop);
 //   startStopBtn.addEventListener("click", Start);
@@ -96,6 +49,7 @@ createApp({
         },
       ],
       activeIndex: 0,
+      isActiveBoo: false,
     };
   },
   methods: {
@@ -113,7 +67,7 @@ createApp({
       } else {
         this.activeIndex = this.images.length - 1;
       }
-    }
+    },
+
   }
 }).mount("#app");
-
